834032ce1b5391a89fd846b73637834606518c8d
fureya48 <fathur.rohman2535@gmail.com>
2022-09-22 17:36:53 +0700

    save project

--- models/usergame.js
+++ models/usergame.js
@@ -1,23 +1,24 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class UserGame extends Model {
     static associate(models) {
-      UserGame.hasOne(models.UserBiodata, {foreignKey: 'UserGameId'})
-      UserGame.hasMany(models.UserHistory, {foreignKey: 'UserGameId'})
+      UserGame.hasOne(models.UserBiodata, { foreignKey: "UserGameId" });
+      UserGame.hasMany(models.UserHistory, { foreignKey: "UserGameId" });
     }
   }
-  UserGame.init({
-    username: {
-      type: DataTypes.STRING,
-      unique: true
+  UserGame.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: DataTypes.STRING,
     },
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'UserGame',
-  });
+    {
+      sequelize,
+      modelName: "UserGame",
+    }
+  );
   return UserGame;
 };
